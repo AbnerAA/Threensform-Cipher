f297540f616122b56835ee3bbef3388b888518e5
AbnerAA <abneradhiwijna@gmail.com>
2019-03-12 22:11:34 +0700

    base for 5 modes

--- cipher.py
+++ cipher.py
@@ -40,16 +40,17 @@ def feistel(block, external_key, cipher_function, iters=12, mode=1, encrypt=True
     return utility.join_block_halves(left_half, right_half)
 
 def block_cipher(text, external_key, cipher_function, block_length=12, iters=12, mode=1, encrypt=True):
-    block_count = math.ceil(1.0*len(text)/block_length)
-    i = 0
-    new_text = ""
-
-    feedback = [0 for i in range(block_length)]
-    while i < block_count:
-        block = text[(i*block_length):((i+1)*block_length)]
-        new_block = feistel(block, external_key, cipher_function, iters, mode, encrypt)
-        new_text = new_text + new_block
-        i += 1
+    
+    if mode == 1:
+        new_text = block_cipher_ecb(text, external_key, cipher_function, block_length=12, iters=12, encrypt=True)
+    elif mode == 2:
+        new_text = block_cipher_cbc(text, external_key, cipher_function, block_length=12, iters=12, encrypt=True)
+    elif mode == 3:
+        new_text = block_cipher_cfb(text, external_key, cipher_function, block_length=12, iters=12, encrypt=True)
+    elif mode == 4:
+        new_text = block_cipher_ofb(text, external_key, cipher_function, block_length=12, iters=12, encrypt=True)
+    elif mode == 5:
+        new_text = block_cipher_counter(text, external_key, cipher_function, block_length=12, iters=12, encrypt=True)
 
     return new_text
 
@@ -106,7 +107,7 @@ def block_cipher_cfb(text, external_key, cipher_function, block_length=12, iters
     while i < block_count:
         block = text[(i*block_length):((i+1)*block_length)]
 
-        new_block = feistel(feedback, external_key, cipher_function, iters, mode, encrypt)
+        new_block = feistel(feedback, external_key, cipher_function, iters, mode, True)
 
         new_block = string_xor(block, new_block)
 
@@ -131,7 +132,7 @@ def block_cipher_ofb(text, external_key, cipher_function, block_length=12, iters
 
     while i < block_count:
         block = text[(i*block_length):((i+1)*block_length)]
-        feedback = feistel(feedback, external_key, cipher_function, iters, mode, encrypt)
+        feedback = feistel(feedback, external_key, cipher_function, iters, mode, True)
         new_block = string_xor(block, feedback)
         new_text = new_text + new_block
         i += 1
